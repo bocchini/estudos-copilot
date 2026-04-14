7c648049afd0ca032d8083cf4319a006b89974df
Américo <albocchini@gmail.com>
2026-04-14 19:43:26 -0300

    Swagger funcionando

--- dotnet/testes/Copilot.Testes.API/Extensions/AppConfigureExtensions.cs
+++ dotnet/testes/Copilot.Testes.API/Extensions/AppConfigureExtensions.cs
@@ -9,19 +9,5 @@ public static class AppConfigureExtensions
     public static void Configure(this WebApplication app)
     {
         app.MapEndpoints();
-        SwaggerMap(app);
     }
-
-    private static void SwaggerMap(WebApplication app)
-    {
-        app.UseSwaggerUI();
-        app.UseSwaggerUI(options =>
-        {
-            options.SwaggerEndpoint("/v1/swagger.json", "Copilot.Testes.API v1");
-            options.DisplayRequestDuration();
-            options.DocExpansion(Swashbuckle.AspNetCore.SwaggerUI.DocExpansion.None);
-            options.ShowCommonExtensions();
-        });
-    }
-
 }
--- dotnet/testes/Copilot.Testes.API/Program.cs
+++ dotnet/testes/Copilot.Testes.API/Program.cs
@@ -6,11 +6,18 @@ var builder = WebApplication.CreateBuilder(args);
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
+builder.Services.AddSwaggerGen();
+
 var app = builder.Build();
+app.UseSwagger();
+
+app.UseSwaggerUI(options =>
+           options.SwaggerEndpoint("/openapi/v1.json", "nome da api"));
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
+
     app.MapOpenApi();
 }
 
